987b1eb26a14bbbe2846e7615edb0ce044688898
Halvadar <levanbaidoshvili@gmail.com>
2024-12-09 20:59:23 +0400

    adds mobile view for login

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -5,45 +5,47 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <Box
-  sx={{
-    display: "flex",
-    minHeight: "100vh",
-  }}
->
-  <Box
-    sx={{
-      flex: 1,
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      p: 4,
-    }}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        minHeight: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          p: 4,
+        }}
       >
         {children}
       </Box>
 
-  <Box
-    sx={{
-      flex: 1,
-      bgcolor: "",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      p: 4,
-      backgroundColor: "#a9e2ff94",
-    }}
-  >
-    <Container maxWidth="sm">
-      <Box sx={{ textAlign: "center" }}>
-        <Typography variant="h1" component="h1" gutterBottom>
-          Welcome Back
-        </Typography>
-        <Typography variant="h6" color="text.secondary">
-          Sign in to access your account and continue your journey with us.
-        </Typography>
+      <Box
+        sx={{
+          flex: 1,
+          bgcolor: "",
+          display: { xs: "none", md: "flex" },
+          justifyContent: "center",
+          alignItems: "center",
+          p: 4,
+          backgroundColor: "#a9e2ff94",
+        }}
+      >
+        <Container maxWidth="sm">
+          <Box sx={{ textAlign: "center" }}>
+            <Typography variant="h1" component="h1" gutterBottom>
+              Welcome Back
+            </Typography>
+            <Typography variant="h6" color="text.secondary">
+              Sign in to access your account and continue your journey with us.
+            </Typography>
+          </Box>
+        </Container>
       </Box>
-    </Container>
-  </Box>
-</Box>
+    </Box>
+  );
 }
