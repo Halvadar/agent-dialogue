42786d01284358ef27d230ca00a0aacf62d0ccb4
Halvadar <levanbaidoshvili@gmail.com>
2025-02-07 19:09:54 +0400

    add deepseek support

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,21 @@
-import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
+let modelProvider: () => LanguageModelV1;
+
+switch (process.env.MODEL_PROVIDER) {
+  case "openai":
+    modelProvider = await import("@ai-sdk/openai").then(
+      (m) => () => m.openai("gpt-4o-mini")
+    );
+    break;
+  case "deepseek":
+    modelProvider = await import("@ai-sdk/deepseek").then(
+      (m) => () => m.deepseek("deepseek-chat") as LanguageModelV1
+    );
+    break;
+  default:
+    throw new Error("Invalid model provider");
+}
+
+import { LanguageModelV1, streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -7,7 +23,7 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages, systemMessage } = await req.json();
   const result = streamText({
-    model: openai("gpt-4o-mini"),
+    model: modelProvider(),
     system: `Your instructions are: "${systemMessage}" \n\n Be concise and to the point. Try to engage and ask questions to the user. Also be responsive to the user's questions.`,
     messages,
     maxTokens: 150,
